7abab3af0b6a5d800ff2b62bbd0773194865ba73
Srikrishnan Sengottai Kasi <sr284605@dal.ca>
2021-03-30 17:14:49 +0530

    Integrating parser with update executor

--- distributed-database/src/main/java/database/QueryParser.java
+++ distributed-database/src/main/java/database/QueryParser.java
@@ -1,219 +1,219 @@
 package database;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 import org.apache.commons.lang3.StringUtils;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class QueryParser {
 
-	private static String databaseName;
+    private static String databaseName;
 
-	public void parsingQuery(String query) {
+    public void parsingQuery(String query) {
 
-		if (query != null && query.trim().length() > 0) {
+        if (query != null && query.trim().length() > 0) {
 
-			query = query.trim();
+            query = query.trim();
 
-			query.replaceAll("\\s+", " ");
+            query.replaceAll("\\s+", " ");
 
-			Pattern pattern = Pattern.compile(
-					"^(SELECT |UPDATE |INSERT |DELETE |CREATE TABLE |CREATE DATABASE |USE DATABASE )",
-					Pattern.CASE_INSENSITIVE);
-			Matcher matcher = pattern.matcher(query);
-			boolean matchFound = matcher.find();
-			if (matchFound) {
-				String queryType = matcher.group();
-				System.out.println("Match found");
+            Pattern pattern = Pattern.compile(
+                    "^(SELECT |UPDATE |INSERT |DELETE |CREATE TABLE |CREATE DATABASE |USE DATABASE )",
+                    Pattern.CASE_INSENSITIVE);
+            Matcher matcher = pattern.matcher(query);
+            boolean matchFound = matcher.find();
+            if (matchFound) {
+                String queryType = matcher.group();
+                System.out.println("Match found");
 
-				if (queryType.trim().equalsIgnoreCase("CREATE DATABASE")) {
-					tokenizeCreateDatabaseQuery(pattern, matcher, query);
-				} else if (queryType.trim().equalsIgnoreCase("USE DATABASE")) {
-					tokenizeUseDatabaseQuery(pattern, matcher, query);
-				} else if (StringUtils.isBlank(QueryParser.databaseName)) {
-					System.out.println("Please specify database");
-				} else if (queryType.trim().equalsIgnoreCase("SELECT")) {
-					tokenizeSelectQuery(pattern, matcher, query);
-				} else if (queryType.trim().equalsIgnoreCase("UPDATE")) {
-					tokenizeUpdateQuery(pattern, matcher, query);
-				} else if (queryType.trim().equalsIgnoreCase("INSERT")) {
-					tokenizeInsertQuery(pattern, matcher, query);
-				} else if (queryType.trim().equalsIgnoreCase("DELETE")) {
-					tokenizeDeleteQuery(pattern, matcher, query);
-				} else if (queryType.trim().equalsIgnoreCase("CREATE TABLE")) {
-					tokenizeCreateTableQuery(pattern, matcher, query);
-				}
-			} else {
-				System.out.println("Query syntax is not correct, please check keywords spellings and order.");
-			}
-		}
-	}
+                if (queryType.trim().equalsIgnoreCase("CREATE DATABASE")) {
+                    tokenizeCreateDatabaseQuery(pattern, matcher, query);
+                } else if (queryType.trim().equalsIgnoreCase("USE DATABASE")) {
+                    tokenizeUseDatabaseQuery(pattern, matcher, query);
+                } else if (StringUtils.isBlank(QueryParser.databaseName)) {
+                    System.out.println("Please specify database");
+                } else if (queryType.trim().equalsIgnoreCase("SELECT")) {
+                    tokenizeSelectQuery(pattern, matcher, query);
+                } else if (queryType.trim().equalsIgnoreCase("UPDATE")) {
+                    tokenizeUpdateQuery(pattern, matcher, query);
+                } else if (queryType.trim().equalsIgnoreCase("INSERT")) {
+                    tokenizeInsertQuery(pattern, matcher, query);
+                } else if (queryType.trim().equalsIgnoreCase("DELETE")) {
+                    tokenizeDeleteQuery(pattern, matcher, query);
+                } else if (queryType.trim().equalsIgnoreCase("CREATE TABLE")) {
+                    tokenizeCreateTableQuery(pattern, matcher, query);
+                }
+            } else {
+                System.out.println("Query syntax is not correct, please check keywords spellings and order.");
+            }
+        }
+    }
 
-	private void tokenizeSelectQuery(Pattern pattern, Matcher matcher, String query) {
-		pattern = Pattern.compile("(SELECT)\\s+(\\*|[\\w, ]+)\\s+FROM\\s+([\\w]+)\\s*( WHERE\\s+(\\S)+)?$",
-				Pattern.CASE_INSENSITIVE);
-		matcher = pattern.matcher(query);
+    private void tokenizeSelectQuery(Pattern pattern, Matcher matcher, String query) {
+        pattern = Pattern.compile("(SELECT)\\s+(\\*|[\\w, ]+)\\s+FROM\\s+([\\w]+)\\s*( WHERE\\s+(\\S)+)?$",
+                Pattern.CASE_INSENSITIVE);
+        matcher = pattern.matcher(query);
 
-		if (matcher.find()) {
-			String operation = matcher.group(1);
-			String columns = matcher.group(2);
-			String tableName = matcher.group(3);
-			String conditions = matcher.group(5);
-			SelectQueryExecutor sqe = new SelectQueryExecutor(tableName, databaseName);
-			sqe.executeSelectMain(operation, columns, conditions);
-		} else {
-			System.out.println("Query syntax is not correct, please check keywords spellings and order.");
-		}
-	}
+        if (matcher.find()) {
+            String operation = matcher.group(1);
+            String columns = matcher.group(2);
+            String tableName = matcher.group(3);
+            String conditions = matcher.group(5);
+            SelectQueryExecutor sqe = new SelectQueryExecutor(tableName, databaseName);
+            sqe.executeSelectMain(operation, columns, conditions);
+        } else {
+            System.out.println("Query syntax is not correct, please check keywords spellings and order.");
+        }
+    }
 
-	private void tokenizeUpdateQuery(Pattern pattern, Matcher matcher, String query) {
-		pattern = Pattern.compile("(UPDATE)\\s+([\\w]+)\\s+SET\\s+(.+)\\s*( WHERE\\s+(\\S)+)?$",
-				Pattern.CASE_INSENSITIVE);
-		matcher = pattern.matcher(query);
+    private void tokenizeUpdateQuery(Pattern pattern, Matcher matcher, String query) {
+        pattern = Pattern.compile("(UPDATE)\\s+([\\w]+)\\s+SET\\s+(.+)\\s*( WHERE\\s+(\\S)+)?$",
+                Pattern.CASE_INSENSITIVE);
+        matcher = pattern.matcher(query);
 
-		if (matcher.find()) {
-			String operation = matcher.group(1);
-			String tableName = matcher.group(2);
-			String[] updateOperations = matcher.group(2).split(",");
-			String conditions = matcher.group(5);
-			UpdateQueryExecutor uqe = new UpdateQueryExecutor(tableName, databaseName);
-			uqe.executeUpdateMain(updateOperations, conditions);
-		} else {
-			System.out.println("Query syntax is not correct, please check keywords spellings and order.");
-		}
-	}
+        if (matcher.find()) {
+            String operation = matcher.group(1);
+            String tableName = matcher.group(2);
+            String updateOperations = matcher.group(2);
+            String conditions = matcher.group(5);
+            UpdateQueryExecutor uqe = new UpdateQueryExecutor(tableName, databaseName);
+            uqe.executeUpdateMain(updateOperations, conditions);
+        } else {
+            System.out.println("Query syntax is not correct, please check keywords spellings and order.");
+        }
+    }
 
-	private void tokenizeInsertQuery(Pattern pattern, Matcher matcher, String query) {
-		pattern = Pattern.compile(
-				"(INSERT)\\s+INTO\\s+([\\w]+)\\s*(\\(([\\w, ]+)\\))?\\s*\\s+VALUES\\s+\\(([\\w, ]+)\\)$",
-				Pattern.CASE_INSENSITIVE);
-		matcher = pattern.matcher(query);
+    private void tokenizeInsertQuery(Pattern pattern, Matcher matcher, String query) {
+        pattern = Pattern.compile(
+                "(INSERT)\\s+INTO\\s+([\\w]+)\\s*(\\(([\\w, ]+)\\))?\\s*\\s+VALUES\\s+\\(([\\w, ]+)\\)$",
+                Pattern.CASE_INSENSITIVE);
+        matcher = pattern.matcher(query);
 
-		if (matcher.find()) {
-			String operation = matcher.group(1);
-			String tableName = matcher.group(2);
-			String[] columnNames = matcher.group(4).split(",");
-			String[] columnValues = matcher.group(5).split(",");
-		} else {
-			System.out.println("Query syntax is not correct, please check keywords spellings and order.");
-		}
-	}
+        if (matcher.find()) {
+            String operation = matcher.group(1);
+            String tableName = matcher.group(2);
+            String[] columnNames = matcher.group(4).split(",");
+            String[] columnValues = matcher.group(5).split(",");
+        } else {
+            System.out.println("Query syntax is not correct, please check keywords spellings and order.");
+        }
+    }
 
-	private void tokenizeDeleteQuery(Pattern pattern, Matcher matcher, String query) {
-		pattern = Pattern.compile("(DELETE)\\s+FROM\\s+([\\w]+)\\s*( WHERE\\s+(\\S)+)?$", Pattern.CASE_INSENSITIVE);
-		matcher = pattern.matcher(query);
+    private void tokenizeDeleteQuery(Pattern pattern, Matcher matcher, String query) {
+        pattern = Pattern.compile("(DELETE)\\s+FROM\\s+([\\w]+)\\s*( WHERE\\s+(\\S)+)?$", Pattern.CASE_INSENSITIVE);
+        matcher = pattern.matcher(query);
 
-		if (matcher.find()) {
-			String operation = matcher.group(1);
-			String tableName = matcher.group(2);
-			String conditions = matcher.group(4);
-		} else {
-			System.out.println("Query syntax is not correct, please check keywords spellings and order.");
-		}
-	}
+        if (matcher.find()) {
+            String operation = matcher.group(1);
+            String tableName = matcher.group(2);
+            String conditions = matcher.group(4);
+        } else {
+            System.out.println("Query syntax is not correct, please check keywords spellings and order.");
+        }
+    }
 
-	private void tokenizeCreateTableQuery(Pattern pattern, Matcher matcher, String query) {
-		pattern = Pattern.compile("(CREATE)\\s+(TABLE)\\s+([\\w]+)\\s*\\(([\\w, \\(\\)]+)\\)$",
-				Pattern.CASE_INSENSITIVE);
-		matcher = pattern.matcher(query);
+    private void tokenizeCreateTableQuery(Pattern pattern, Matcher matcher, String query) {
+        pattern = Pattern.compile("(CREATE)\\s+(TABLE)\\s+([\\w]+)\\s*\\(([\\w, \\(\\)]+)\\)$",
+                Pattern.CASE_INSENSITIVE);
+        matcher = pattern.matcher(query);
 
-		if (matcher.find()) {
-			String operation = matcher.group(1);
-			String subOperation = matcher.group(2);
-			String tableName = matcher.group(3);
-			String columnsDesc = matcher.group(4);
+        if (matcher.find()) {
+            String operation = matcher.group(1);
+            String subOperation = matcher.group(2);
+            String tableName = matcher.group(3);
+            String columnsDesc = matcher.group(4);
 
-			String[] columnDescArray = columnsDesc.split(",");
+            String[] columnDescArray = columnsDesc.split(",");
 
-			JSONObject tableColumnsObject = new JSONObject();
-			JSONObject tableForeignKeysObject = new JSONObject();
-			String primaryKey = null;
+            JSONObject tableColumnsObject = new JSONObject();
+            JSONObject tableForeignKeysObject = new JSONObject();
+            String primaryKey = null;
 
-			JSONArray foreignKeyArray = new JSONArray();
-			JSONArray columnArray = new JSONArray();
-			JSONObject foreignKeyObj = null;
-			JSONObject columnObj = null;
+            JSONArray foreignKeyArray = new JSONArray();
+            JSONArray columnArray = new JSONArray();
+            JSONObject foreignKeyObj = null;
+            JSONObject columnObj = null;
 
-			for (String columnDesc : columnDescArray) {
-				pattern = Pattern.compile(
-						"([\\w]+)\\s*( INT| VARCHAR)\\s*( PRIMARY KEY| REFERENCES\\s+([\\w]+)\\((\\w+)\\))?\\s*$",
-						Pattern.CASE_INSENSITIVE);
-				matcher = pattern.matcher(columnDesc);
-				if (matcher.find()) {
-					columnObj = new JSONObject();
-					String columnName = matcher.group(1);
-					String columnType = matcher.group(2);
-					String constraint = matcher.group(3);
-					String foreignKeyTable = matcher.group(4);
-					String foreignKeyColumn = matcher.group(5);
+            for (String columnDesc : columnDescArray) {
+                pattern = Pattern.compile(
+                        "([\\w]+)\\s*( INT| VARCHAR)\\s*( PRIMARY KEY| REFERENCES\\s+([\\w]+)\\((\\w+)\\))?\\s*$",
+                        Pattern.CASE_INSENSITIVE);
+                matcher = pattern.matcher(columnDesc);
+                if (matcher.find()) {
+                    columnObj = new JSONObject();
+                    String columnName = matcher.group(1);
+                    String columnType = matcher.group(2);
+                    String constraint = matcher.group(3);
+                    String foreignKeyTable = matcher.group(4);
+                    String foreignKeyColumn = matcher.group(5);
 
-					columnObj.put("columnName", columnName.trim());
-					columnObj.put("columnType", columnType.trim());
-					columnArray.add(columnObj);
+                    columnObj.put("columnName", columnName.trim());
+                    columnObj.put("columnType", columnType.trim());
+                    columnArray.add(columnObj);
 
-					if (constraint != null && constraint.trim().length() > 0
-							&& constraint.trim().equalsIgnoreCase("PRIMARY KEY")) {
-						primaryKey = columnName.trim();
-					}
+                    if (constraint != null && constraint.trim().length() > 0
+                            && constraint.trim().equalsIgnoreCase("PRIMARY KEY")) {
+                        primaryKey = columnName.trim();
+                    }
 
-					if (foreignKeyTable != null && foreignKeyTable.trim().length() > 0) {
-						foreignKeyObj = new JSONObject();
-						foreignKeyObj.put("column", columnName);
-						foreignKeyObj.put("foreignKeyTable", foreignKeyTable.trim());
-						foreignKeyObj.put("foreignKeyColumn", foreignKeyColumn.trim());
-						foreignKeyArray.add(foreignKeyObj);
-					}
+                    if (foreignKeyTable != null && foreignKeyTable.trim().length() > 0) {
+                        foreignKeyObj = new JSONObject();
+                        foreignKeyObj.put("column", columnName);
+                        foreignKeyObj.put("foreignKeyTable", foreignKeyTable.trim());
+                        foreignKeyObj.put("foreignKeyColumn", foreignKeyColumn.trim());
+                        foreignKeyArray.add(foreignKeyObj);
+                    }
 
-				} else {
-					System.out.println("Please check syntax of the command.");
-				}
-			}
-			tableColumnsObject.put("columns", columnArray);
-			tableForeignKeysObject.put("keys", foreignKeyArray);
-			CreateTableQuery createTableQuery = new CreateTableQuery();
-			createTableQuery.exceuteCreateTableQuery(QueryParser.databaseName, tableName, primaryKey,
-					tableColumnsObject, tableForeignKeysObject);
+                } else {
+                    System.out.println("Please check syntax of the command.");
+                }
+            }
+            tableColumnsObject.put("columns", columnArray);
+            tableForeignKeysObject.put("keys", foreignKeyArray);
+            CreateTableQuery createTableQuery = new CreateTableQuery();
+            createTableQuery.exceuteCreateTableQuery(QueryParser.databaseName, tableName, primaryKey,
+                    tableColumnsObject, tableForeignKeysObject);
 
-		} else {
-			System.out.println("Query syntax is not correct, please check keywords spellings and order.");
-		}
-	}
+        } else {
+            System.out.println("Query syntax is not correct, please check keywords spellings and order.");
+        }
+    }
 
-	private void tokenizeCreateDatabaseQuery(Pattern pattern, Matcher matcher, String query) {
-		pattern = Pattern.compile("(CREATE)\\s+(DATABASE)\\s+([\\w]+)\\s*$", Pattern.CASE_INSENSITIVE);
-		matcher = pattern.matcher(query);
+    private void tokenizeCreateDatabaseQuery(Pattern pattern, Matcher matcher, String query) {
+        pattern = Pattern.compile("(CREATE)\\s+(DATABASE)\\s+([\\w]+)\\s*$", Pattern.CASE_INSENSITIVE);
+        matcher = pattern.matcher(query);
 
-		if (matcher.find()) {
-			String databaseName = matcher.group(3);
-			CreateDatabaseQuery createDatabaseQuery = new CreateDatabaseQuery();
-			createDatabaseQuery.exceuteCreateDatabaseQuery(databaseName);
-		} else {
-			System.out.println("Query syntax is not correct, please check keywords spellings and order.");
-		}
-	}
+        if (matcher.find()) {
+            String databaseName = matcher.group(3);
+            CreateDatabaseQuery createDatabaseQuery = new CreateDatabaseQuery();
+            createDatabaseQuery.exceuteCreateDatabaseQuery(databaseName);
+        } else {
+            System.out.println("Query syntax is not correct, please check keywords spellings and order.");
+        }
+    }
 
-	private void tokenizeUseDatabaseQuery(Pattern pattern, Matcher matcher, String query) {
-		pattern = Pattern.compile("(USE)\\s+(DATABASE)\\s+([\\w]+)\\s*$", Pattern.CASE_INSENSITIVE);
-		matcher = pattern.matcher(query);
+    private void tokenizeUseDatabaseQuery(Pattern pattern, Matcher matcher, String query) {
+        pattern = Pattern.compile("(USE)\\s+(DATABASE)\\s+([\\w]+)\\s*$", Pattern.CASE_INSENSITIVE);
+        matcher = pattern.matcher(query);
 
-		if (matcher.find()) {
-			String databaseName = matcher.group(3);
-			QueryParser.databaseName = databaseName;
-		} else {
-			System.out.println("Query syntax is not correct, please check keywords spellings and order.");
-		}
-	}
+        if (matcher.find()) {
+            String databaseName = matcher.group(3);
+            QueryParser.databaseName = databaseName;
+        } else {
+            System.out.println("Query syntax is not correct, please check keywords spellings and order.");
+        }
+    }
 
-	public static void main(String[] args) {
-		QueryParser parser = new QueryParser();
-		String query = "CREATE DATABASE test";
-		parser.parsingQuery(query);
-		query = "USE DATABASE test";
-		parser.parsingQuery(query);
-		query = "CREATE TABLE Persons_2 (PersonID int PRIMARY KEY,LastName varchar, FirstName varchar REFERENCES Persons(PersonID), Address varchar, City varchar)";
-		parser.parsingQuery(query);
+    public static void main(String[] args) {
+        QueryParser parser = new QueryParser();
+        String query = "CREATE DATABASE test";
+        parser.parsingQuery(query);
+        query = "USE DATABASE test";
+        parser.parsingQuery(query);
+        query = "CREATE TABLE Persons_2 (PersonID int PRIMARY KEY,LastName varchar, FirstName varchar REFERENCES Persons(PersonID), Address varchar, City varchar)";
+        parser.parsingQuery(query);
 
-	}
+    }
 }
--- distributed-database/src/main/java/database/UpdateQueryExecutor.java
+++ distributed-database/src/main/java/database/UpdateQueryExecutor.java
@@ -3,6 +3,7 @@ package database;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -21,7 +22,7 @@ public class UpdateQueryExecutor {
     }
 
     BufferedReader getTableReader() throws Exception {
-        String tablePath = this.databaseName + '/' + this.tableName;
+        String tablePath = this.databaseName + '/' + this.tableName + ".txt";
         return new BufferedReader(new FileReader(tablePath));
     }
 
@@ -44,41 +45,64 @@ public class UpdateQueryExecutor {
     int getIndexOfKeyName(String keyName) {
         populateColumnMap();
         for (Entry<Integer, String> columns : fieldMap.entrySet()) {
-            if (columns.getValue().toString().equals(keyName)) {
+            if (columns.getValue().equalsIgnoreCase(keyName)) {
                 return (int) columns.getKey();
             }
         }
         return -1;
     }
 
-    public boolean performUpdateOnARow(String keyName, String keyValue, String updateKey, String updateValue) {
+    public void performUpdateOnARow(String keyName, String keyValue, String updateKey, String updateValue, int compOperation) {
         SelectQueryExecutor selectQueryExecutor = new SelectQueryExecutor(tableName, databaseName);
         Map<Integer, List<String>> resultSet = selectQueryExecutor.executeSelectStatementWithFullColumns();
         int indexOfKey = getIndexOfKeyName(keyName);
         int indexOfUpdatedColumn = getIndexOfKeyName(updateKey);
-        if (indexOfKey == -1) {
+        if (indexOfKey == -1 || indexOfUpdatedColumn == -1) {
             throw new IllegalArgumentException("Invalid key constraint");
         }
         for (Entry<Integer, List<String>> rows : resultSet.entrySet()) {
-            List<String> rowDetails = rows.getValue();
-            String presentValue = rowDetails.get(indexOfKey);
-            if (presentValue.equalsIgnoreCase(updateValue)) {
-                rowDetails.add(indexOfUpdatedColumn, updateValue);
+            String[] rowVal;
+            rowVal = new String[rows.getValue().size()];
+            rows.getValue().toArray(rowVal);
+            String presentValue = rows.getValue().get(indexOfKey);
+            if (constraintCheck(presentValue, keyValue, compOperation)) {
+                rowVal[indexOfUpdatedColumn] = updateValue;
             }
-            resultSet.put(rows.getKey(), rowDetails);
+            resultSet.put(rows.getKey(), Arrays.asList(rowVal));
         }
-        return insertRowDetailsInFile(resultSet);
+        insertRowDetailsInFile(resultSet);
+    }
+
+    private boolean constraintCheck(String presentValue, String keyValue, int compOperation) {
+        switch (compOperation) {
+            case 0:
+                return presentValue.equalsIgnoreCase(keyValue);
+            case 1:
+                return Integer.parseInt(presentValue) > Integer.parseInt(keyValue);
+            case 2:
+                return Integer.parseInt(presentValue) < Integer.parseInt(keyValue);
+            case 3:
+                return !presentValue.equalsIgnoreCase(keyValue);
+            case 4:
+                return Integer.parseInt(presentValue) >= Integer.parseInt(keyValue);
+            case 5:
+                return Integer.parseInt(presentValue) <= Integer.parseInt(keyValue);
+            default:
+                return false;
+
+        }
     }
 
     private boolean insertRowDetailsInFile(Map<Integer, List<String>> resultSet) {
         try {
-            try (FileWriter insertRecord = new FileWriter(databaseName + "/" + tableName)) {
-                String headerLine = String.join("\\$", fieldMap.values());
+            try (FileWriter insertRecord = new FileWriter(databaseName + "/" + tableName + ".txt")) {
+                String headerLine = String.join("$", fieldMap.values());
+                System.out.println(headerLine);
                 insertRecord.write(headerLine);
                 insertRecord.write("\n");
                 for (Entry<Integer, List<String>> rows : resultSet.entrySet()) {
                     List<String> rowData = rows.getValue();
-                    String line = String.join("\\$", rowData);
+                    String line = String.join("$", rowData);
                     insertRecord.write(line);
                     insertRecord.write("\n");
                 }
@@ -90,8 +114,73 @@ public class UpdateQueryExecutor {
         return false;
     }
 
-    void executeUpdateMain(String[] operations, String condition) {
+    void executeUpdateMain(String operations, String condition) {
+        if (operations != null && condition != null) {
+            String[] operationPairs = operations.split(",");
+            for (String operation : operationPairs) {
+                String[] operands = operation.split("=");
+                int comparisonOperation = fetchOperationType(condition);
+                String operator = fetchOperation(condition);
+                String[] conditionParameters = condition.split(operator);
+                System.out.println(conditionParameters[0]);
+                performUpdateOnARow(conditionParameters[0], conditionParameters[1],
+                        operands[0], operands[1], comparisonOperation);
+            }
+            System.out.println("Updated successfully in the table!");
+        }
 
+    }
+
+    private int fetchOperationType(String condition) {
+        if (condition.contains("!=")) {
+            return 3;
+        }
+        if (condition.contains(">=")) {
+            return 4;
+        }
+        if (condition.contains("<=")) {
+            return 5;
+        }
+        if (condition.contains("=")) {
+            return 0;
+        }
+        if (condition.contains(">")) {
+            return 1;
+        }
+        if (condition.contains("<")) {
+            return 2;
+        }
+
+        return -1;
+    }
+
+    private String fetchOperation(String condition) {
+        if (condition.contains(">=")) {
+            return ">=";
+        }
+        if (condition.contains("!=")) {
+            return "!=";
+        }
+        if (condition.contains("<=")) {
+            return "<=";
+        }
+        if (condition.contains("=")) {
+            return "=";
+        }
+        if (condition.contains(">")) {
+            return ">";
+        }
+        if (condition.contains("<")) {
+            return "<";
+        }
+        return "";
+    }
+
+    public static void main(String[] args) {
+        UpdateQueryExecutor uqe = new UpdateQueryExecutor("students", "test");
+        String operations = "csci5408=30";
+        String conditions = "English>=90";
+        uqe.executeUpdateMain(operations, conditions);
     }
 
 }
