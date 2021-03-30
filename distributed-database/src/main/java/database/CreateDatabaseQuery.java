394be92aadc0deb8f1bf292b6b14272ef36c097d
Srikrishnan Sengottai Kasi <sr284605@dal.ca>
2021-03-30 17:32:11 +0530

    Removing unwanted class

--- distributed-database/src/main/java/database/CreateDatabaseQuery.java
+++ distributed-database/src/main/java/database/CreateDatabaseQuery.java
@@ -4,17 +4,17 @@ import java.io.File;
 
 public class CreateDatabaseQuery {
 
-	public void exceuteCreateDatabaseQuery(String database) {
-		File databaseFolder = new File("Database\\"+ database);
-		if (!databaseFolder.exists()){
-			databaseFolder.mkdirs();
-		}
-	}
-	
-	public static void main(String[] args) {
-		System.out.println("Started");
-		CreateDatabaseQuery CreateDatabaseQuery = new CreateDatabaseQuery();
-		CreateDatabaseQuery.exceuteCreateDatabaseQuery("test3");	
-	}
+    public void exceuteCreateDatabaseQuery(String database) {
+        File databaseFolder = new File("Database\\" + database);
+        if (!databaseFolder.exists()) {
+            databaseFolder.mkdirs();
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Started");
+        CreateDatabaseQuery CreateDatabaseQuery = new CreateDatabaseQuery();
+        CreateDatabaseQuery.exceuteCreateDatabaseQuery("test3");
+    }
 
 }
