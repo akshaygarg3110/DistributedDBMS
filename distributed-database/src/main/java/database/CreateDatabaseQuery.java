1081c8b0124bf31ce1ba2415a9dc869944e2ed8d
Akshay Garg <ak561005@dal.ca>
2021-04-03 17:14:59 -0300

    Revert "commit Added columns in file"
    This reverts commit 52265a5ace7429e93b191e118f693e68838ee61d

--- distributed-database/src/main/java/database/CreateDatabaseQuery.java
+++ distributed-database/src/main/java/database/CreateDatabaseQuery.java
@@ -4,7 +4,6 @@ import java.io.File;
 
 public class CreateDatabaseQuery {
 
-<<<<<<< HEAD
     public void exceuteCreateDatabaseQuery(String database) {
         File databaseFolder = new File("Database\\" + database);
         if (!databaseFolder.exists()) {
@@ -17,19 +16,5 @@ public class CreateDatabaseQuery {
         CreateDatabaseQuery CreateDatabaseQuery = new CreateDatabaseQuery();
         CreateDatabaseQuery.exceuteCreateDatabaseQuery("test3");
     }
-=======
-	public void exceuteCreateDatabaseQuery(String database) {
-		File databaseFolder = new File("Database\\"+ database);
-		if (!databaseFolder.exists()){
-			databaseFolder.mkdirs();
-		}
-	}
-	
-	public static void main(String[] args) {
-		System.out.println("Started 1");
-		CreateDatabaseQuery CreateDatabaseQuery = new CreateDatabaseQuery();
-		CreateDatabaseQuery.exceuteCreateDatabaseQuery("test3");	
-	}
->>>>>>> 2e65b5b (Added columns in file)
 
 }
